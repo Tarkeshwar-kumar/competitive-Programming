9e1986da34caa57fc5bfb946d81ece3051fafb4b
tarak <ktarkeshwar305@gmail.com>
2021-11-20 01:38:13 +0530

    dfs added

---
+++ graph/theory/dfs.cpp
@@ -0,0 +1,22 @@
+//
+// Created by admin on 20/11/2021.
+//
+
+//dfs
+/*
+ *Depth First Search finds the lexicographical first path in the graph from a source vertex u to each vertex
+ *Depth First Search will also find the shortest paths in a tree (because there only exists one simple path), but on
+ *general graphs this is not the case.
+ *
+ *The algorithm works in O(m+n) time when n is the number of vertices and m is the number of edges.
+ *
+ *
+ *
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+}
